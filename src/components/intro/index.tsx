f925d566971032f8b0080a5156eb8666a175a11e
raviprasad-aeq <raviprasad@aequalisys.com>
2023-10-02 15:47:08 +0530

    Updated resume

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -47,7 +47,7 @@ const Intro = () => {
                 disableRipple
                 onClick={() =>
                   openInNewTab(
-                    "https://drive.google.com/file/d/1m2RRnyUSfbtRswS3vBHENdPpZTnkCdxS/view?usp=drive_link",
+                    "https://drive.google.com/file/d/1LkgV8JyHY8Cuhd9KmrWLQJKVCfdvz9ih/view?usp=drive_link",
                   )
                 }
               >
